ffd1f1c8f369170c31ec1d8d3b4ee9029f1229fe
Pysched <48335713+Pysched@users.noreply.github.com>
2023-10-27 12:04:53 +0100

    changed something

--- src/main/java/org/example/MyControler.java
+++ src/main/java/org/example/MyControler.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyControler {
     @GetMapping("/")
     public String index(){
-        return "Greetings from Spring Boot, with Web Hook! Now testing mid run - and it stops the previous run!";
+        return "Is this working?"   ;
     }
 }
