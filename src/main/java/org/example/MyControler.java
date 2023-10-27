e4452e055fbfd0f56a45d23b5aa11e0802e657e0
Pysched <48335713+Pysched@users.noreply.github.com>
2023-10-27 15:27:39 +0100

    text changed

--- src/main/java/org/example/MyControler.java
+++ src/main/java/org/example/MyControler.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyControler {
     @GetMapping("/")
     public String index(){
-        return "Is this working? it might be working! now nowdfgsdrfgsdfgsdf";
+        return "Is this working? it might be working! now ";
     }
 }
